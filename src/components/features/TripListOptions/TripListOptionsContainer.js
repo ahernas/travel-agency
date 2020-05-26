314b716958b3ed8f4329d1cb97053cf626361bc3
Anna Garczarek <ania.garczarek@gmail.com>
2020-05-27 01:15:36 +0200

    Start new project, implement routing and redux

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,18 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, changeDuration, changeTags} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  // TODO - add more dispatchers for other filters
+  changeDuration: (type, value) => dispatch(changeDuration({type, value})),
+  changeTags: (tags) => dispatch(changeTags(tags)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
